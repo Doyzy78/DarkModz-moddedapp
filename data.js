4b678fda8a896a2b2e81c6f43c3d03830cfac1c6
Doyzy78 <162144547+Doyzy78@users.noreply.github.com>
2025-07-04 19:50:20 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const appData = [
+  {
+    id: "NetMirror",
+    name: "NetMirror Pro",
+    version: "3.2",
+    size: "150 MB",
+    icon: "Net.png",
+    apk: "NetMirror_3.2.apk",
+    developer: "NetMirror. / Modded by Joker Devs",
+    updated: "July 2025", 
+    modFeatures: [
+      "✅ No Ads",
+      "✅ No Login Problem",
+      "✅ All Feature Unlocked"
+    ]
+  }
+];
