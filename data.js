39959afb79d86d9d8d2ba28d5460c0da16115048
Doyzy78 <162144547+Doyzy78@users.noreply.github.com>
2025-07-25 19:33:57 +0800

    data.js

--- data.js
+++ data.js
@@ -59,13 +59,74 @@ const appData = [
       "✅ Fully Accurate"
     ]
   },
+const appData = [
+  {
+    id: "GG TOOLS",
+    name: "GG TOOLS Pro",
+    version: "2.0",
+    size: "15 MB",
+    icon: "icon.png",
+    apk: "GG TOOLS_2.0.apk",
+    developer: "GG TOOLS / Developed by Joker Devs",
+    updated: "July 2025",
+    modFeatures: [
+      "✅ Value to Arm",
+      "✅ AutoUpdate Script",
+      "✅ SCRIPT Normal",
+      "✅ Menu Builder"
+    ]
+  },
+  {
+    id: "Typing Engine",
+    name: "Typing Engine",
+    version: "2.0",
+    size: "10 MB",
+    icon: "type.png",
+    apk: "Typing Engine_2.0.apk",
+    developer: "Typing Engine / Developed by Joker Devs",
+    updated: "July 2025",
+    modFeatures: [
+      "✅ Automatic Typing",
+      "✅ Accurate Value Input"
+    ]
+  },
+  {
+    id: "Sb HookGen",
+    name: "Sb HookGen Pro",
+    version: "4.0",
+    size: "15 MB",
+    icon: "Sb.png",
+    apk: "Sb HookGen_4.0.apk",
+    developer: "Sb HookGen / Developed by Sb Mods",
+    updated: "July 2025",
+    modFeatures: [
+      "✅ Normal Hooking",
+      "✅ Field Hooking",
+      "✅ Patch Generator",
+      "✅ Auto Update Hooking",
+      "✅ SeekBar Hooking"
+    ]
+  },
+  {
+    id: "Hex Engine",
+    name: "Hex Engine Pro",
+    version: "4.0",
+    size: "15 MB",
+    icon: "Hex.png",
+    apk: "Hex Engine_4.0.apk",
+    developer: "Hex Engine / Developed by @Gura Mods",
+    updated: "July 2025",
+    modFeatures: [
+      "✅ Fully Accurate"
+    ]
+  },
   {
     id: "subway-surf-32bit",
     name: "[Subway Surf] [32BIT]",
     version: "AutoUpdate",
     size: "2 MB",
     icon: "subway.png",
-    apk: "Subway Surf [32 BIT].lua",
+    apk: "Script/Subway Surf [32 BIT].lua",
     developer: "DarkWolf",
     updated: "July 2025",
     modFeatures: [
@@ -104,7 +165,53 @@ const appData = [
       "🛹 Invisible Hoverboard",
       "🧮 Min Lane Change Speed",
       "🏃‍♂️ High Jump Boost"
-    ],
-    directDownload: "https://www.mediafire.com/file/mhcnpm13kj8dfo8/Subway+Surf+[32+BIT].lua/file"
+    ]
+  },
+  {
+    id: "subway-surf-64bit",
+    name: "[Subway Surf] [64BIT]",
+    version: "AutoUpdate",
+    size: "2 MB",
+    icon: "subway.png",
+    apk: "Subway Surf [64 BIT].lua",
+    developer: "DarkWolf",
+    updated: "July 2025",
+    modFeatures: [
+      "🌟 Infinite Stars",
+      "💰 Remove Coin Pickup",
+      "🕊️ Fly Mode",
+      "⚡ Run Fast",
+      "🔋 Unlimited Power",
+      "🚄 Pass Through Trains",
+      "🚄 Pass Through Trains (2)",
+      "🧍‍♂️ Walk While Flying",
+      "🎁 Everything Unlimited",
+      "🛒 Internal Purchases",
+      "🆓 Free Purchases",
+      "🏆 Unlock Achievements",
+      "💸 Double Coins",
+      "🎥 Follow Camera",
+      "🛡️ No Impact",
+      "🛡️ No Impact (2)",
+      "🛡️ No Impact (3)",
+      "💀 Immortal Mode",
+      "💀 Immortal Mode (2)",
+      "🌌 Gravity Off",
+      "❄️ Freeze Screen",
+      "🚀 Infinite Jump",
+      "🔁 Fast Lane Change",
+      "👤 Unlock Characters",
+      "👤 Unlock Characters (2)",
+      "👤 Unlock Characters (3)",
+      "👤 Unlock Characters (4)",
+      "🧭 Teleport",
+      "👟 Invisible Super Sneakers",
+      "🚀 Invisible Jetpack",
+      "🦘 Invisible Pogo Stick",
+      "🧲 Invisible Magnet",
+      "🛹 Invisible Hoverboard",
+      "🧮 Min Lane Change Speed",
+      "🏃‍♂️ High Jump Boost"
+    ]
   }
 ];
